a9c30780b01b47810cea73b9bca9b124822aab30
Carlos Mendez <carlos@hadaelectronics.com>
2020-08-20 16:22:00 -0500

    adding merge arrays algorithm 2

--- Algorithms-JS/039_merge_arrays/main.js
+++ Algorithms-JS/039_merge_arrays/main.js
@@ -5,15 +5,13 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
     let i = m - 1;
-	let j = n - 1;
-	let k = m + n - 1;
- 
-	while (k >= 0) {
-		if (j < 0 || (i >= 0 && nums1[i] > nums2[j]))
-			nums1[k--] = nums1[i--];
-		else
-			nums1[k--] = nums2[j--];
-	}
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (k >= 0) {
+        if (j < 0 || (i >= 0 && nums1[i] > nums2[j])) nums1[k--] = nums1[i--];
+        else nums1[k--] = nums2[j--];
+    }
 };
