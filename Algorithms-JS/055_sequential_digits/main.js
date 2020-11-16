5a72f7aa8d08c536ff821df2662e16a9e1e17190
Carlos Mendez <carlos@hadaelectronics.com>
2020-11-16 15:51:51 -0600

    another approach

--- Algorithms-JS/055_sequential_digits/main.js
+++ Algorithms-JS/055_sequential_digits/main.js
@@ -4,31 +4,73 @@
  * @return {number[]}
  */
 var sequentialDigits = function (low, high) {
-    let i = low;
-    const results = [];
+	let i = low;
+	const results = [];
 
-    while (high >= i) {
-        const currNum = i.toString().split("");
-        const res = checkNum(currNum);
-        if (res !== undefined) results.push(i);
-        i++;
-    }
+	while (high >= i) {
+		const currNum = i.toString().split("");
+		const res = checkNum(currNum);
+		if (res !== undefined) results.push(i);
+		i++;
+	}
 
-    return results;
+	return results;
 };
 
 var checkNum = function (num) {
-    let temp;
-    for (let i = 0; num.length > i; i++) {
-        const currNum = +num[i] + 1;
-        if (temp === undefined) {
-            temp = currNum;
-            continue;
+	let temp;
+	for (let i = 0; num.length > i; i++) {
+		const currNum = +num[i] + 1;
+		if (temp === undefined) {
+			temp = currNum;
+			continue;
+		}
+
+		if (num[i] != temp) return undefined;
+		temp = currNum;
+	}
+
+	return num;
+};
+
+
+
+
+
+
+
+/**
+ * VERSION II`
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    const result = [];
+    
+    for (let i = low; i <= high; i++) {
+        const currNum = [...i.toString()];
+        let temp = undefined;
+        let f = false;
+        for (let j = 0; j < currNum.length; j++) {
+            const cn = +currNum[j];
+            if (temp === undefined) {
+                temp = cn; 
+                continue;
+            }
+            
+            if (cn === (temp + 1)) {
+                f = true;
+            } else {
+                f = false;
+                break;
+            }
+            
+            temp = cn
         }
-
-        if (num[i] != temp) return undefined;
-        temp = currNum;
+        
+        if (f) result.push(+currNum.join(""));
     }
-
-    return num;
+    
+    return result;
 };
